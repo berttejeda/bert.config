a99b9403a89669b0990e15c386a2bfabdc46b1a1
Tejeda, Engelbert <etejeda@seic.com>
2021-08-09 16:02:30 -0400

    Removed unused ordered_load method

--- bertdotconfig/__init__.py
+++ bertdotconfig/__init__.py
@@ -18,17 +18,6 @@ class SuperDuperConfig():
     self.DictUtils = DictUtils()
     self.extra_config_search_paths = kwargs.get('extra_config_search_paths', '')
 
-  def ordered_load(self, stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
-    class OrderedLoader(Loader):
-      pass
-    def construct_mapping(loader, node):
-      loader.flatten_mapping(node)
-      return object_pairs_hook(loader.construct_pairs(node))
-    OrderedLoader.add_constructor(
-      yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-      construct_mapping)
-    return yaml.load(stream, OrderedLoader)            
-
   def load_config(self, **kwargs):
     """Load specified config file"""
 
