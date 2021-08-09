e376e87013022dc761fce41c1c9742fbb2538257
Tejeda, Engelbert <etejeda@seic.com>
2021-08-09 13:48:51 -0400

    Improved config file search logic

--- bertdotconfig/__init__.py
+++ bertdotconfig/__init__.py
@@ -14,9 +14,9 @@ class SuperDuperConfig():
 
   def __init__(self, **kwargs):
     self.config_name = kwargs.get('config_name', '')
-    self.config_path = kwargs.get('config_path', '')
     self.logger = logger
     self.DictUtils = DictUtils()
+    self.extra_config_search_paths = kwargs.get('extra_config_search_paths', '')
 
   def ordered_load(self, stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
     class OrderedLoader(Loader):
@@ -29,27 +29,38 @@ class SuperDuperConfig():
       construct_mapping)
     return yaml.load(stream, OrderedLoader)            
 
-  def load_config(self, config_file=None, req_keys=[], failfast=False, data_key=None, debug=False, as_object=False):
+  def load_config(self, config_file_name=None, config_file_path=None, req_keys=[], failfast=False, data_key=None, debug=False, as_object=False):
     """Load specified config file"""
-    config_path_strings = [
-      os.path.realpath(os.path.expanduser('~')),
-      '.',
-      os.path.join(os.path.abspath(os.sep), 'etc')
-    ]
-    config_paths = [self.config_path]
-    if config_file:
-      config_paths += [os.path.join(p, config_file)
-        for p in config_path_strings]
-      config_paths.append(os.path.expanduser(config_file))
+    if not config_file_path:
+      config_search_paths = [
+        os.path.realpath(os.path.expanduser('~')),
+        '.',
+        os.path.join(os.path.abspath(os.sep), 'etc')
+      ]
+      if self.extra_config_search_paths:
+        if isinstance(self.extra_config_search_paths, list):
+          config_search_paths += self.extra_config_search_paths
+        elif isinstance(self.extra_config_search_paths, str):
+          config_search_paths += [self.extra_config_search_paths]
+        else:
+          self.logger.error('extra_config_search_paths must be of type str or list')
+          sys.exit(1)
+      if config_file_name:
+        config_file_paths = [
+        os.path.expanduser(os.path.join(p, config_file_name))
+          for p in config_search_paths
+          ]
+    else:
+      config_file_paths = [config_file_path]
     config_found = False
     config_is_valid = False
-    for config_path in config_paths:
-      config_exists = os.path.exists(config_path)
+    for config_file_path in config_file_paths:
+      config_exists = os.path.exists(config_file_path)
       if config_exists:
         config_found = True
-        self.config_path = config_path
+        self.config_file_path = config_file_path
         try:
-          with open(config_path, 'r') as ymlfile:
+          with open(config_file_path, 'r') as ymlfile:
             # Preserve dictionary order for python 2
             # https://stackoverflow.com/questions/5121931/in-python-how-can-you-load-yaml-mappings-as-ordereddicts
             if sys.version_info[0] < 3:
@@ -57,31 +68,31 @@ class SuperDuperConfig():
             else:
               cfg = yaml.load(ymlfile, yaml.Loader)
           config_dict = cfg[data_key] if data_key is not None else cfg
-          config_dict['config_path'] = self.config_path
+          config_dict['config_file_path'] = self.config_file_path
           config_is_valid = all([m[m.keys()[0]].get(k) for k in req_keys for m in config_dict])
           self.logger.debug(
-            "Found input file - {cf}".format(cf=config_path))
+            "Found input file - {cf}".format(cf=config_file_path))
           if not config_is_valid:
-            logger.warning(
+            self.logger.warning(
               """At least one required key was not defined in your input file: {cf}.""".format(
-              cf=config_path)
+              cf=config_file_path)
             )
             self.logger.warning(
               "Review the available documentation or consult --help")
-          config_file = config_path
+          config_file_path = config_file_path
           break
         except Exception as e:
           self.logger.warning(
           "I encountered a problem reading your input file: {cp}, error was {err}".format(
-          cp=config_path, err=str(e))
+          cp=config_file_path, err=str(e))
           )
     if not config_found:
       if failfast:
-        self.logger.error("Could not find %s. Aborting." % config_file)
+        self.logger.error("Could not find %s. Aborting." % config_file_path)
         sys.exit(1)
       else:
         self.logger.debug(
-        "Could not find %s, not loading values" % config_file)
+        "Could not find %s, not loading values" % config_file_path)
 
     if config_found and config_is_valid:
       if as_object:
@@ -91,7 +102,7 @@ class SuperDuperConfig():
     else:
       if failfast:
         self.logger.error(
-        "Config %s is invalid. Aborting." % config_file)
+        "Config %s is invalid. Aborting." % config_file_path)
         sys.exit(1)
       else:
         if as_object:
