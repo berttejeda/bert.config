558c6543bbad45346379d638636bf398d578fd9f
Bert Tejeda <etejeda@tecknicos.com>
2022-06-27 00:25:31 -0400

    Refactor custom dict properties

--- bertdotconfig/configutils.py
+++ bertdotconfig/configutils.py
@@ -5,9 +5,52 @@ from functools import reduce
 logger = Logger().init_logger(__name__)
 
 class AttrDict(dict):
-    def __init__(self, *args, **kwargs):
-        super(AttrDict, self).__init__(*args, **kwargs)
-        self.__dict__ = self
+    """
+    see: https://stackoverflow.com/questions/3797957/python-easily-access-deeply-nested-dict-get-and-set
+    """
+    def __init__(self, value=None):
+        if value is None:
+            pass
+        elif isinstance(value, dict):
+            for key in value:
+                self.__setitem__(key, value[key])
+        else:
+            raise(TypeError, 'expected dict')
+
+    def __setitem__(self, key, value):
+        if '.' in key:
+            myKey, restOfKey = key.split('.', 1)
+            target = self.setdefault(myKey, AttrDict())
+            if not isinstance(target, AttrDict):
+                raise(KeyError, 'cannot set "%s" in "%s" (%s)' % (restOfKey, myKey, repr(target)))
+            target[restOfKey] = value
+        else:
+            if isinstance(value, dict) and not isinstance(value, AttrDict):
+                value = AttrDict(value)
+            dict.__setitem__(self, key, value)
+
+    def __getitem__(self, key):
+        if '.' not in key:
+            return dict.__getitem__(self, key)
+        myKey, restOfKey = key.split('.', 1)
+        target = dict.__getitem__(self, myKey)
+        if not isinstance(target, AttrDict):
+            raise(KeyError, 'cannot get "%s" in "%s" (%s)' % (restOfKey, myKey, repr(target)))
+        return target[restOfKey]
+
+    def __contains__(self, key):
+        if '.' not in key:
+            return dict.__contains__(self, key)
+        myKey, restOfKey = key.split('.', 1)
+        target = dict.__getitem__(self, myKey)
+        if not isinstance(target, AttrDict):
+            return False
+        return restOfKey in target
+
+    def setdefault(self, key, default):
+        if key not in self:
+            self[key] = default
+        return self[key]
 
     def merge(self, incoming_dct, **kwargs):
         """ Recursive dict merge. Inspired by :meth:``dict.update()``, instead of
@@ -37,5 +80,10 @@ class AttrDict(dict):
             result = reduce(lambda d, key: d.get(key, default) if isinstance(
                 d, dict) else default, dict_path.split('.'), self.dict)
         return(result)
+
+    __setattr__ = __setitem__
+    __getattr__ = __getitem__
+
+
 
        
